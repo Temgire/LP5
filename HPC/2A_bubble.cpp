19e94141c7c42e7556deccb8cd15fa6481eeea62
god <71623952+MAHIRE-7@users.noreply.github.com>
2025-04-29 09:16:38 +0530

    Add files via upload

--- HPC/2A_bubble.cpp
+++ HPC/2A_bubble.cpp
@@ -1,64 +1,45 @@
 #include <iostream>
-#include <vector>
-#include <stack>
 #include <omp.h>
 using namespace std;
 
-const int MAX = 100000;
-vector<int> graph[MAX]; // Adjacency list
-bool visited[MAX]; // Visited array to track visited nodes
+void parallelBubbleSort(int arr[], int n) {
+    bool swapped = true;
 
-// DFS function
-void dfs(int start_node) {
-    stack<int> s;
-    s.push(start_node); // Push the starting node to the stack
+    for (int i = 0; i < n && swapped; i++) {
+        swapped = false;
 
-    while (!s.empty()) {
-        int curr_node = s.top();
-        s.pop();
-
-        // If the node has not been visited, mark it as visited
-        if (!visited[curr_node]) {
-            visited[curr_node] = true;
-            cout << curr_node << " "; // Print the visited node
+        // EVEN phase
+        #pragma omp parallel for
+        for (int j = 0; j < n - 1; j += 2) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
         }
 
-        // Add adjacent nodes to the stack
-        for (int i = 0; i < graph[curr_node].size(); i++) {
-            int adj_node = graph[curr_node][i];
-            if (!visited[adj_node]) {
-                s.push(adj_node);
+        // ODD phase
+        #pragma omp parallel for
+        for (int j = 1; j < n - 1; j += 2) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
             }
         }
     }
 }
 
 int main() {
-    int n, m, start_node;
+    int arr[] = {5, 3, 8, 4, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    // Input for number of nodes, edges, and start node
-    cout << "Enter the number of nodes, edges, and the start node: ";
-    cin >> n >> m >> start_node;
+    cout << "Initial array: ";
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    cout << endl;
 
-    cout << "Enter the pairs of edges:" << endl;
+    parallelBubbleSort(arr, n);
 
-    // Input for the edges and constructing the graph
-    for (int i = 0; i < m; i++) {
-        int u, v;
-        cin >> u >> v;
-        graph[u].push_back(v);
-        graph[v].push_back(u); // Undirected graph
-    }
-
-    // Parallel initialization of the visited array using OpenMP
-    #pragma omp parallel for
-    for (int i = 0; i < n; i++) {
-        visited[i] = false;
-    }
-
-    // Call DFS starting from the given node
-    cout << "DFS Traversal starting from node " << start_node << ": ";
-    dfs(start_node);
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
     cout << endl;
 
     return 0;
